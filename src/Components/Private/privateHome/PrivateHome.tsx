2e92caa13729cd8e05d4e23516b7db3b108f0a4e
Micael Jerry <hei.jerry.2@gmail.com>
2022-08-18 21:57:36 +0300

    update application

---
+++ src/Components/Private/privateHome/PrivateHome.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './PrivateHome.css';
+
+function PrivateHome() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/PrivateHome.tsx</code> and save to reload.
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default PrivateHome;
